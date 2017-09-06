22d4787146a519a57c3ad25864bbcc63f158b1b6
XOR-IND\phuge_k <kirtiphuge7585@gmail.com>
2017-09-06 17:33:08 +0530

    lists and collections

---
+++ List/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8_List
+{
+    public class Employee
+    {
+        public string name;
+        public int empcode;
+        public int salary;
+
+        public Employee(int _id,string _name,int _sal)
+        {
+            empcode = _id;
+            name = _name;
+            salary = _sal;
+        }
+
+        public override string ToString()
+        {
+            return "Person: " + empcode + " " + name + " " + salary;
+        }
+    }
+}
---
+++ List/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> li = new List<Employee>();
+
+            Employee e1 = new Employee(101, "Kirti", 100000);
+            Employee e2 = new Employee(102, "Kirti", 10000);
+            Employee e3 = new Employee(103, "Kirti", 20000);
+            Employee e4 = new Employee(104, "Kirti", 300000);
+            Employee e5 = new Employee(105, "Kirti", 100000);
+            li.Add(e1);
+            li.Add(e2);
+            li.Add(e3);
+            li.Add(e4);
+            li.Add(e5);
+
+            foreach (Employee o in li.OrderBy(e=>e.salary))
+            {
+                //Console.Write("\t"+o.ID);
+                //Console.Write("\t"+o.name);
+                //Console.Write("\t"+o.dept_name);
+                Console.WriteLine(o);
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
