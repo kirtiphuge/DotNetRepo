b471fbe049cf553e3b3d8d75daa8215b7a3e86ab
XOR-IND\phuge_k <kirtiphuge7585@gmail.com>
2017-09-06 17:37:05 +0530

    Core ADO.NET Database First  Approach

---
+++ DatabaseFirstApproach/SnehaEmp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnehaDemoClass
+{
+    public class SnehaEmp
+    {
+        public int Empid { get; set; }
+
+        public String Empname{ get;set;}
+
+        public int Salary{ get;set;}
+    }
+}
