fa5269e6f016232ba03f9e300dcfba25059766a6
XOR-IND\phuge_k <kirtiphuge7585@gmail.com>
2017-09-06 17:29:41 +0530

    Generics

---
+++ Generics/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7_Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SwapGenerics<int> s1 = new SwapGenerics<int>();
+            SwapGenerics<string> s2 = new SwapGenerics<string>();
+            s1.Swap(10, 20);
+            s2.Swap("A", "B");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Generics/SwapGenerics.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7_Generics
+{
+    class SwapGenerics<T>
+    {
+        public void Swap(T a,T b)
+        {
+            T temp;
+            temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine("After Swap : \n First No : {0} \t Second No : {1}", a, b);
+        }
+    }
+}
