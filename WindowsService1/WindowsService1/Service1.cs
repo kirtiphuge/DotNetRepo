343a3ea503bf3319a081a589d2112d80c8dcaed9
XOR-IND\phuge_k <kirtiphuge7585@gmail.com>
2017-09-07 09:55:15 +0530

    Windows Service

---
+++ WindowsService1/WindowsService1/Service1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace WindowsService1
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+
+            if(!System.Diagnostics.EventLog.SourceExists("ServiceSource"))
+            {
+                System.Diagnostics.EventLog.CreateEventSource("ServiceSource", "ServiceLog");
+            }
+
+            eventLog1.Source = "ServiceSource";
+            eventLog1.Log = "ServiceLog";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog1.WriteEntry("My Service Started");
+
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Interval = 60000;  // 60 seconds
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(this.onTimer);
+            timer.Start();
+        }
+
+        private void onTimer(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            eventLog1.WriteEntry("Monitoring the System", EventLogEntryType.Information);
+        }
+
+        protected override void OnStop()
+        {
+            eventLog1.WriteEntry("My Service Stoped");
+        }
+    }
+}
